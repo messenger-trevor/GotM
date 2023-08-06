2cebcacfeb3e6293c8be5ebc5206c9eb65a36b2f
Trevor Messenger <messenger.trevor@gmail.com>
2023-08-05 20:40:26 -0600

    version 0.0.1 initial commit. Adds current dependencies and file structure for project.

---
+++ src/main/java/com/trevormessenger/GotM/GotM/GotMApplication.java
@@ -0,0 +1,13 @@
+package com.trevormessenger.GotM.GotM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trevormessenger/GotM/GotM/GotMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trevormessenger.GotM.GotM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
